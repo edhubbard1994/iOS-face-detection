97be7f873ebe31e73d0146f71126fbab1c63bfd3
Ed Hubbard <Edhubbard1994@gmail.com>
2020-08-03 22:49:48 -0500

    ui setup

--- face-detection/ViewController.swift
+++ face-detection/ViewController.swift
@@ -10,12 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var imageView: UIImageView?
+    var nextButton: UIButton?
+    var previousButton: UIButton?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.imageView = imageViewSetup()
+        self.nextButton = setupNextButton()
     }
     
-    func getImage() -> UIImage? {
+    func setupNextButton() -> UIButton {
+        let btn = UIButton(frame:
+            CGRect(x: self.view.frame.width / 2,
+                   y: 4 * self.view.frame.height / 5,
+                width: self.view.frame.width / 2,
+                height: 20)
+        )
+        btn.setTitle("Next", for: .normal)
+        btn.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)
+        self.view.bringSubviewToFront(btn)
+        self.view.addSubview(btn)
+        return btn
+    }
+    
+    func imageViewSetup() -> UIImageView {
+        let view = UIImageView(frame: self.view.bounds)
+        self.view.addSubview(view)
+        return view;
+    }
+    
+    private func getImage() -> UIImage? {
         return nil
     }
 
