e1afe764a4ad1cc4e4782c1168f1ebea9fbfd0c3
Ed Hubbard <Edhubbard1994@gmail.com>
2020-08-03 21:51:30 -0500

    Initial Commit

---
+++ face-detection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  face-detection
+//
+//  Created by Edward Hubbard on 8/3/20.
+//  Copyright © 2020 Edward Hubbard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
